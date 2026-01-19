809d74fb5f1b9386cb4c1da385d74e970e232c5a
Sarim Ali <sarimali8549@gmail.com>
2026-01-19 16:21:49 +0530

    code cleanup

--- firebase.ts
+++ firebase.ts
@@ -5,13 +5,13 @@ import { getStorage } from 'firebase/storage';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCTKSduZcqmkN2osU6aH5ZaH4rfYO5KQNA",
-  authDomain: "closure-2026.firebaseapp.com",
-  projectId: "closure-2026",
-  storageBucket: "closure-2026.firebasestorage.app",
-  messagingSenderId: "340167651716",
-  appId: "1:340167651716:web:18779909d3e70fe0126dd1",
-  measurementId: "G-GZ9NMNXSS6"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
